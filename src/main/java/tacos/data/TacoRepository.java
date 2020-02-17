b310280863d8af28bc99a625972fa4d8ca1b274a
Kamil Wojcikowski <vojcio24@gmail.com>
2020-02-17 14:35:16 +0100

    Approach with use of HATEOAS

--- src/main/java/tacos/data/TacoRepository.java
+++ src/main/java/tacos/data/TacoRepository.java
@@ -1,9 +1,9 @@
 package tacos.data;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import tacos.Taco;
 
-public interface TacoRepository extends CrudRepository<Taco, Long> {
+public interface TacoRepository extends PagingAndSortingRepository<Taco, Long> {
 
 }
--- src/main/java/tacos/web/api/DesignTacoController.java
+++ src/main/java/tacos/web/api/DesignTacoController.java
@@ -1,18 +1,20 @@
 package tacos.web.api;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -34,10 +36,37 @@ public class DesignTacoController {
 		this.tacoRepo = tacoRepo;
 	}
 	
+	/*
+	 * @GetMapping("/recent") public Iterable<Taco> recentTacos() { PageRequest page
+	 * = PageRequest.of(0, 12, Sort.by("createdAt").descending()); return
+	 * tacoRepo.findAll(page).getContent(); }
+	 */
+	
+	/*
+	 * @GetMapping("/recent") public Resources<Resource<Taco>> recentTacos() {
+	 * PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+	 * 
+	 * List<Taco> tacos = tacoRepo.findAll(page).getContent();
+	 * Resources<Resource<Taco>> recentResources = Resources.wrap(tacos);
+	 * 
+	 * //recentResources.add(new Link("http://localhost:8080/design/recent",
+	 * "recents"));
+	 * recentResources.add(ControllerLinkBuilder.linkTo(DesignTacoController.class).
+	 * slash("recent").withRel("recents"));
+	 * //recentResources.add(ControllerLinkBuilder.linkTo(methodOn(
+	 * DesignTacoController.class).recentTacos()).withRel("recents")); return
+	 * recentResources; }
+	 */
+	
 	@GetMapping("/recent")
-	public Iterable<Taco> recentTacos() {
-		PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
-		return tacoRepo.findAll(page).getContent();
+	public Resources<TacoResource> recentTacos() {
+		PageRequest page = PageRequest.of(0,  12, Sort.by("createdAt").descending());
+		List<Taco> tacos = tacoRepo.findAll(page).getContent();
+		List<TacoResource> tacoResources = new TacoResourceAssembler().toResources(tacos);
+		Resources<TacoResource> recentResources = new Resources<TacoResource>(tacoResources);
+		recentResources
+				.add(ControllerLinkBuilder.linkTo(DesignTacoController.class).slash("recent").withRel("recents"));
+		return recentResources;
 	}
 	
 	@GetMapping("/{id}")
